85e0e590bbe34a62b6b733f27d0277d5d57054eb
ovoaaahj <v_v33s@naver.com>
2024-10-14 22:09:23 +0900

    행렬의 곱

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,9 @@ import static book.array.arrayTest.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,3,2,4,2};
-        int[] sortArr = examCase2(arr);
+        int[][] arr1 = {{2,3,2},{4,2,4},{3,4,1}};
+        int[][] arr2 =  {{5,4,3},{2,4,1},{3,1,1}};
+        int[][] sortArr = matrixMultiplication(arr1,arr2);
 //        System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(sortArr));
     }
--- src/book/array/arrayTest.java
+++ src/book/array/arrayTest.java
@@ -116,4 +116,22 @@ public class arrayTest {
 
         return answer;
     }
+
+    public static int[][] matrixMultiplication(int[][] arr1 , int[][] arr2){
+        int arr1oneSize = arr1.length;
+        int arr1twoSize = arr1[0].length;
+        int arr2twoSize = arr2[0].length;
+
+        int[][] answer = new int[arr1oneSize][arr2twoSize];
+        for(int i = 0; i<arr1oneSize;i++){
+            for(int j=0;j<arr2twoSize;j++){
+                for(int k=0;k<arr1twoSize;k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        
+        return answer;
+    }
+
 }
